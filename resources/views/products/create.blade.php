e78fdace97a66e981610af4d7791f57321c43dfe
pimenvibritania <blackbrit13@gmail.com>
2018-01-22 21:30:10 +0700

    first

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+	<div class="container">
+		<h2>Create a Product</h2>
+		<form method="post">
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="form-group col-md-4">
+					<label for="name">Name : </label>
+					<input type="text" class="form-control" name="name">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="form-group col-md-4">
+					<label for="price">Price : </label>
+					<input type="text" class="form-control" name="price">
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="form-group col-md-4">
+					<button type="submit" class="btn btn-success" style="margin-left: 38px">Add Product</button>
+				</div>
+			</div>
+		</form>
+	</div>
+@endsection
